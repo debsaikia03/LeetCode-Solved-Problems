6f9a59ad096d615f0f988606b72c2c04e2bb2ca1
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2024-05-31 10:44:54 +0530

    Time: 96 ms (46.85%), Space: 73 MB (63.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std :: unordered_set<int> seen;
+        unordered_set<int> seen;
 
         for(int i : nums){
             if(seen.find(i) != seen.end()){
