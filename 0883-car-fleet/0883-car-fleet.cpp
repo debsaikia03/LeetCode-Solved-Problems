6dc118d2e1f248e37fa5e8607ec0ca442474510a
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2024-07-17 11:11:34 +0530

    Time: 118 ms (90.22%), Space: 82.5 MB (42.87%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.cpp
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
 
-        int n = position.size();
+        int n = position.size(); // n -> no. of cars
 
-        stack<float> st;
-        vector<pair<int,float>> vec;
+        stack<float> st;  //initialize stack
+        vector<pair<int,float>> vec;  //pair storing position & time to reach
          
         for(int i=0; i<n; i++){
-            vec.push_back({position[i] , float(target-position[i])/speed[i]});
+            vec.push_back({position[i] , float(target-position[i])/speed[i]}); //stores the position and time
         }
 
-        sort(vec.begin() , vec.end());
+        sort(vec.begin() , vec.end()); //sorts the vector by first element
 
-        for(auto it:vec){
+        for(auto it:vec){ //auto increment vector
 
-            float time = it.second;
+            float time = it.second; //stores the time to which the iteratir currently points at
 
-            while(st.size() && (time >= st.top())) st.pop();
+            while(st.size() && (time >= st.top())) st.pop(); //st.size-> ensures that the stack is not empty //if top element is less than current time it is pushed //if top element greater than or equal to current time it is being popped and current time is pushed 
 
-            st.push(time);
+        st.push(time); 
         }
 
         return st.size();
