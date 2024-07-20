179c952d78088ead27c9f1a7cbfbea4bd001f9cd
debsaikia03 <143481057+debsaikia03@users.noreply.github.com>
2024-07-20 22:24:17 +0530

    Time: 44 ms (10.39%), Space: 21.8 MB (17.28%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int minSpeed = 1;
+        
+        //get max pile
+        int maxSpeed = *max_element(piles.begin(), piles.end());
+
+
+        while(minSpeed < maxSpeed){
+            int mid = minSpeed + (maxSpeed-minSpeed) / 2;
+
+            if(canEatPiles(piles, h, mid)){
+                maxSpeed = mid;
+            }
+            else{
+                minSpeed = mid + 1;
+            }
+        }
+
+        return minSpeed;
+    }
+    
+private:
+    bool canEatPiles(const std::vector<int>& piles, int h, int speed){
+        int hours = 0;
+
+        for(int i: piles){
+            hours += std::ceil(static_cast<double>(i) / speed);
+        }
+
+        return hours <= h;
+    }
+    
+};
