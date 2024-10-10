3213543baa8c3795e7cfcb1510137b3a92245db4
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2024-10-10 20:54:52 +0530

    Time: 10 ms (31.49%), Space: 49.1 MB (12.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,9 +2,11 @@ class Solution {
     public int change(int amount, int[] coins) {
 
         int temp[][] = new int[coins.length+1][amount+1];
+
         for(int i=0; i <= coins.length; i++){
             temp[i][0] = 1;
         }
+
         for(int i=1; i <= coins.length; i++){
             for(int j=1; j <= amount ; j++){
                 if(coins[i-1] > j){
@@ -15,6 +17,7 @@ class Solution {
                 }
             }
         }
+        
         return temp[coins.length][amount];
     }
 }
