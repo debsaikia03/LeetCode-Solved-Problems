191a5b20c10e5d55bba0576ff119dc7118624051
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-01-18 23:03:15 +0530

    Time: 13 ms (21.5%), Space: 14.4 MB (30.72%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.c
@@ -0,0 +1,63 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *next;
+ *     struct Node *random;
+ * };
+ */
+
+struct Node* copyRandomList(struct Node* head) {
+
+    if (!head){
+
+        return NULL;
+    }
+	
+    struct Node* temp = head;
+
+    while(temp != NULL){
+
+        struct Node* copyNode = (struct Node*)malloc(sizeof(struct Node));
+
+        copyNode->val = temp->val;
+        copyNode->next = temp->next;
+        copyNode->random = NULL;
+
+        temp->next = copyNode;
+        temp = copyNode->next;
+    }
+
+    temp = head;
+
+    while(temp != NULL){
+
+        struct Node* copyNode = temp->next;
+
+        if(temp->random != NULL) copyNode->random = temp->random->next;
+
+        temp = copyNode->next;
+    }
+
+    struct Node* dummyNode = (struct Node*)malloc(sizeof(struct Node));
+
+    dummyNode->val = -1;
+    dummyNode->next = NULL;
+    dummyNode->random = NULL;
+
+    struct Node* res = dummyNode;
+    temp = head;
+    
+    while(temp != NULL){
+
+        struct Node* copyNode = temp->next;
+        temp->next = copyNode->next;
+
+        res->next = copyNode;
+
+        res = res->next;
+        temp = temp->next;
+    }
+
+    return dummyNode->next;
+}
