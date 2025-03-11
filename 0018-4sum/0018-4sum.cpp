a3e462918b88b3f89f5394176855dadc3afaa3e8
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-03-11 19:24:51 +0530

    Time: 17 ms (83.85%), Space: 17.6 MB (23.14%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,41 +1,47 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target) {
-        std::vector<std::vector<int>> ans;
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
-        if (nums.size() < 4) return ans; // Edge case
+        vector<vector<int>> ans;
 
-        std::sort(nums.begin(), nums.end()); // Sort the array
+        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+        for(int i = 0; i < nums.size(); i++){
 
-            for (int j = i + 1; j < nums.size() - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // Skip duplicates
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-                int k = j + 1, l = nums.size() - 1;
+            for(int j = i+1; j < nums.size(); j++){
 
-                while (k < l) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int k = j+1;
+                int l = nums.size()-1;
+
+                while(k < l){
+
                     long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
-                    if (sum == target) {
-                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                    if(sum == target){
+
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+
                         k++;
                         l--;
 
-                        // Skip duplicate values of k and l after finding a valid quadruplet
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;                        
                     }
-                    else if (sum < target) k++; // Move to a larger value
-                    else l--; // Move to a smaller value
+
+                    else if(sum < target) k++;
+
+                    else l--;
+
                 }
             }
         }
 
         return ans;
+        
     }
 };
