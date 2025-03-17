f76ac98d7ba9f043c3e1a77e70c3f250762d3ca6
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-03-17 11:55:28 +0530

    Time: 96 ms (28.69%), Space: 48.4 MB (12.14%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    const int N = 1e3+10;
+    const int INF = 1e9+10;
+
+    int dijkstra(int src, int n, vector<pair<int,int>> g[]) {
+        
+        vector<int> vis(N, 0);
+        vector<int> dist(N, INF);
+
+        set<pair<int,int>> st;
+        st.insert({0, src});
+        dist[src] = 0;
+
+        while(!st.empty()) {
+
+            auto node = *st.begin();
+            int v = node.second;
+            int v_dist = node.first;
+            st.erase(st.begin());
+
+            if(vis[v]) continue; // if the node is already visited then continue
+            vis[v] = 1; // mark the node as visited before processing its children
+
+            for(auto child : g[v]) {
+
+                int child_v = child.first;
+                int wt = child.second;
+
+                if(dist[v] + wt < dist[child_v]) {
+
+                    dist[child_v] = dist[v] + wt;
+                    st.insert({dist[child_v], child_v});
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+
+            if(dist[i] == INF) return -1;
+            ans = max(ans, dist[i]);
+        }
+
+        return ans;
+    }
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<pair<int,int>> g[N]; //make a graph from the vector array
+
+        for(auto vec : times){
+
+            g[vec[0]].push_back({vec[1], vec[2]});
+        }
+
+        return dijkstra(k, n, g);
+    }
+};
