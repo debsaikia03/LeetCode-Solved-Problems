693f09b8b3b86b2f0dba2cce3fb3ca7316cb51d7
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-06-11 14:18:38 +0530

    Time: 0 ms (100%), Space: 9.5 MB (54.82%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -11,14 +11,23 @@ public:
     
     void push(int x) {
         
-        q2.push(x);
+        //using 2 queues
+        /*q2.push(x);
 
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
 
-        swap(q1,q2);
+        swap(q1,q2);*/
+
+        //using 1 queue
+        q1.push(x);
+
+        for(int i = 0; i < q1.size()-1; i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
     }
     
     int pop() {
