af065d4c3a6686958ee8b7c694dc28014038cd3a
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-06-24 14:31:45 +0530

    Time: 4 ms (97.41%), Space: 34.9 MB (66.16%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    int helper(vector<int>& w, int cap){
+
+        int days = 1, load = 0;
+
+        for(int i = 0; i < w.size(); i++){
+
+            if(load + w[i] > cap){
+                days++;
+                load = w[i];
+            } else{
+                load += w[i];
+            }
+        }
+ 
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int n = weights.size();
+
+        int low = *max_element(weights.begin(), weights.end()); //max pkg wgt
+
+        int high = accumulate(weights.begin(), weights.end(), 0); //sum of wgts of pkgs
+
+        int ans = high;
+
+        while(low <= high){
+            
+            int mid = low + (high - low)/2;
+
+            int daysReqd = helper(weights, mid);
+
+            if(daysReqd <= days){
+               ans = mid;
+               high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
