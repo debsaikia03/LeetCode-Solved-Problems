6b806aedfbf4230bc0209138fee79868950d4122
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-11 23:08:48 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.9%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     double myPow(double x, int n) {
         
-        return pow(x,n);
+        long long N = n; // Use long long to safely handle INT_MIN
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double ans = 1;
+
+        while(N > 0){
+
+            if(N % 2 == 1){
+                ans = ans * x;
+                N--;
+            }
+
+            else{
+                N = N / 2;
+                x = x * x;
+            }
+        }
+
+        return ans;
     }
 };
