62dc5e442b9e26784846365287dd4fb91de81cc1
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-14 14:55:05 +0530

    Time: 0 ms (100%), Space: 12 MB (33.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,13 +4,11 @@ public:
         
         sort(strs.begin(), strs.end());
 
-        int n = strs.size();
-
         string first = strs[0];
-        string last = strs[n-1];
+        string last = strs[strs.size() - 1];
 
         int i = 0;
-
+        
         while(i < first.size() && first[i] == last[i]) i++;
 
         return first.substr(0,i);
