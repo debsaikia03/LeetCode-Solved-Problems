0c40762f8ded0939aae19ffa09954fe5f435d30c
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-16 13:05:13 +0530

    Time: 8 ms (8.23%), Space: 9.7 MB (48.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,10 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
         
-        unordered_map<char, int> count;
-        
-        // Count characters in s and t
-        for (int i = 0; i < s.size(); i++) {
-            count[s[i]]++;  // Increment count for character in s
-            count[t[i]]--;  // Decrement count for character in t
-        }
-        
-        // Check if all counts are zero
-        for (const auto& kv : count) {
-            if (kv.second != 0) return false;
-        }
-        
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
     }
 };
