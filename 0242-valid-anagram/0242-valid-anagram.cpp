789167654dbc94eae203d3abd141a52e2e1d6078
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-16 14:23:22 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
 
-        return s == t;
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, int> mpp;
+
+        for(int i = 0; i < s.size(); i++){
+
+            mpp[s[i]]++;
+        }
+
+        for(int i = 0; i < t.size(); i++){
+
+            if(mpp.find(t[i]) == mpp.end()  || mpp[t[i]] == 0) return false;
+
+            mpp[t[i]]--;
+        }
+
+        return true;
     }
 };
