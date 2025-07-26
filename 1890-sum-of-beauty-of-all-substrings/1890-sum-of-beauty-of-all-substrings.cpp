eff6ed9a665df1f63cca996c14d874a87c3dd94d
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-26 13:03:50 +0530

    Time: 122 ms (45.22%), Space: 26 MB (37.67%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -4,17 +4,17 @@ public:
         
         int total = 0;
 
-        for(int i = 0; i < s.size(); i++){
-
+        for(int i = 0; i < s.size(); i++){ //O(n)
+ 
             unordered_map<char, int> freq;
 
-            for(int j = i; j < s.size(); j++){
+            for(int j = i; j < s.size(); j++){ //O(n-i) : O(n)
 
                 freq[s[j]]++;
 
                 int maxF = 0, minF = INT_MAX;
 
-                for(auto& [ch, count] : freq){
+                for(auto& [ch, count] : freq){ //O(26) : O(1) 
 
                     maxF = max(maxF, count);
                     minF = min(minF, count);
@@ -27,3 +27,6 @@ public:
         return total;
     }
 };
+
+//tc : O(n^2)
+//sc : O(1) since it only takes O(26)
