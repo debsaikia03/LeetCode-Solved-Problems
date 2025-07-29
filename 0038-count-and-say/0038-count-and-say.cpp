224e607576331f1d5a9cb94c72ff1eb771d24350
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-07-29 23:00:13 +0530

    Time: 3 ms (83.12%), Space: 9.7 MB (81.26%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        string ans = "1";
+
+        if(n == 1) return ans;
+
+        n--;
+
+        while(n--){
+
+            string temp = "";
+            int i = 0;
+
+            while(i < ans.size()){
+ 
+                char curr = ans[i];
+                int count = 0;
+
+                while(i < ans.size() && ans[i] == curr){
+
+                    count++;
+                    i++;
+                }
+
+                temp += to_string(count) + curr;
+            }
+            
+            ans = temp;
+        }
+
+        return ans;
+    }
+};
