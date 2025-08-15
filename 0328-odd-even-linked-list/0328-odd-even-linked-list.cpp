913d7032c8ce5edfa18cc3f0828228c0c6d855e6
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-08-15 16:02:24 +0530

    Time: 0 ms (100%), Space: 15.6 MB (28.62%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -8,27 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
+        // If the list has 0 or 1 node, no rearrangement is needed
         if(!head || !head->next) return head;
 
+        // 'odd' will track the current odd node
+        // 'even' will track the current even node
         ListNode *odd = head, *even = head->next;
 
+        // Save the starting point of even list
+        // This will be attached after odd list ends
         ListNode* evenHead = even;
 
+        // Traverse until even pointer or its next becomes NULL
+        // This ensures we do not go out of bounds
         while(even && even->next){
 
+            // Link current odd node to the next odd node
             odd->next = odd->next->next;
+
+            // Link current even node to the next even node
             even->next = even->next->next;
 
+            // Move odd and even pointers forward
             odd = odd->next;
             even = even->next;
         }
 
+        // Attach the even list after the last odd node
         odd->next = evenHead;
         
+        // Head now represents rearranged list
         return head;
     }
 };
