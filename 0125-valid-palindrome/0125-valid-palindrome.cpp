f595b82fd46750eebcce8126d9e39082e1cf7b73
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-01 14:56:36 +0530

    Time: 0 ms (100%), Space: 10.4 MB (18.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+
+    string removeWhitespace(string s) {
+        string newStr = "";
+        int i = 0;
+        while (i < s.size()) {  
+            if (isalnum(s[i])) {
+                newStr += tolower(s[i]);
+            }
+            i++;
+        }
+        return newStr;
+    }
+    
     bool isPalindrome(string s) {
-        int l = 0;
-        int r = s.size() - 1;
-        
-        while (l < r) {
-            while (!isalnum(s[l]) && l < r) {
-                l++;
-            }
-            while (!isalnum(s[r]) && l < r) {
-                r--;
-            }
-            if (tolower(s[l]) != tolower(s[r])) {
-                return false;
-            }
-            l++;
-            r--;
+        string newS = removeWhitespace(s);
+        int n = newS.size();
+        int i = 0, j = n - 1;
+
+        while (i <= j) {
+            if (newS[i] != newS[j]) return false;
+            i++;
+            j--; 
         }
         return true;
     }
