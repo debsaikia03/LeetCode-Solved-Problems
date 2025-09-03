270f11f9f10fa16bde5cc17d88a2c470d0af8b4f
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-03 17:31:02 +0530

    Time: 796 ms (31.69%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -2,7 +2,7 @@
 SELECT product_id, year AS first_year, quantity, price
 FROM sales 
 WHERE (product_id, year) in (
-    SELECT product_id, MIN(year) as first_year
+    SELECT product_id, MIN(year) 
     FROM sales
     GROUP BY product_id
 )
