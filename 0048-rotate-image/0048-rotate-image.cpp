e8d8c90ce117026eec90d2660cdc8e1fbdb05bf1
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-20 01:05:43 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.44%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -8,6 +8,21 @@ public:
         a = a - b;
     }
 
+    void reverse(vector<vector<int>>& matrix, int i){
+
+        int n = matrix.size() - 1;
+
+        int j = 0;
+
+        while(j < n){
+
+            swap(matrix[i][j], matrix[i][n]);
+
+            j++;
+            n--;
+        }        
+    }
+
     void rotate(vector<vector<int>>& matrix) {
 
         int n = matrix.size();
@@ -19,9 +34,14 @@ public:
             }
         }
         
-        for(int i = 0; i < n; i++){
+        /*for(int i = 0; i < n; i++){
 
             reverse(matrix[i].begin(), matrix[i].end());
+        }*/
+
+        for(int i = 0; i < n; i++){
+            
+            reverse(matrix, i);
         }
     }
 };
