f475ee38b13c0be76d34fb0c273f95f82633b423
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-23 22:53:57 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,19 +3,20 @@ public:
     void moveZeroes(vector<int>& nums) {
 
         int pos = 0;
-
+        
         for(int i = 0; i < nums.size(); i++){
 
             if(nums[i] != 0){
+
                 nums[pos] = nums[i];
                 pos++;
             }
         }
 
         while(pos < nums.size()){
+
             nums[pos] = 0;
             pos++;
         }
-        
     }
 };
