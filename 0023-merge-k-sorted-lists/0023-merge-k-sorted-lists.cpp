c249845454d0ff5c7b3162d1dbaa24f3dfdc3d22
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-25 19:50:39 +0530

    Time: 3 ms (63.79%), Space: 19 MB (21.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,31 +10,38 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        vector<int> res;
+    // Function to merge k sorted linked lists
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    vector<int> res; // Vector to store all node values
 
-        for(auto list : lists){
-
-            while(list){
-
-                res.push_back(list->val);
-                list = list->next;
-            }
+    // Step 1: Extract all elements from each list
+    for (auto list : lists) {
+        while (list) {
+            res.push_back(list->val);
+            list = list->next;
         }
-
-        sort(res.begin(), res.end());
-
-        ListNode* dummy = new ListNode(0);
-
-        ListNode* tail = dummy;
-
-        for(int val : res){
-
-            tail->next = new ListNode(val);
-            tail = tail->next;
-        }
-
-        return dummy->next;
     }
+
+    // Step 2: Sort all values
+    sort(res.begin(), res.end());
+
+    // Step 3: Build the merged list using dummy pointer
+    // Create a dummy node (placeholder) with value 0
+    ListNode* dummy = new ListNode(0);
+
+    // Tail will always point to the last node of the new list
+    ListNode* tail = dummy;
+
+    // Step 4: Insert sorted values into the new linked list
+    for (int val : res) {
+        // Create a new node and attach it to tail->next
+        tail->next = new ListNode(val);
+
+        // Move tail to the newly created node
+        tail = tail->next;
+    }
+
+    // The real head is dummy->next (skip dummy placeholder)
+    return dummy->next;
+}
 };
