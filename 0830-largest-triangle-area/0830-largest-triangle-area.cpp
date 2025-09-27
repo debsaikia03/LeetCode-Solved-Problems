ee3c46950cbedc9a3048b55f3935bfd43253e0fe
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-09-27 12:48:17 +0530

    Time: 1 ms (45.92%), Space: 10.3 MB (86.95%) - LeetHub

---
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    double largestTriangleArea(vector<vector<int>>& points) {
+        
+        int n = points.size();
+        double maxArea = 0.0;
+
+        
+        for(int i = 0; i < n; i++){
+
+            for(int j = i + 1; j < n; j++){
+
+                for(int k = j + 1; k < n; k++){
+
+                    double x1 = points[i][0];
+                    double y1 = points[i][1];
+
+                    double x2 = points[j][0];
+                    double y2 = points[j][1];
+
+                    double x3 = points[k][0];
+                    double y3 = points[k][1];
+
+                    double formula = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)); //shoelace formula
+
+                    maxArea = max(maxArea, formula);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
