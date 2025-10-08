604162abf7040c615f90c21ffb6916e290a32689
Debangshu Saikia <143481057+debsaikia03@users.noreply.github.com>
2025-10-08 11:51:51 +0530

    Time: 39 ms (84.25%), Space: 128.5 MB (5.75%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+
+    int binarySearch(vector<int>& potions, double potionStr){
+
+        int l = 0, r = potions.size(); //may return potions.size() if all elements < potionStr
+
+        while(l < r){
+
+            int mid = l + (r - l) / 2;
+
+            if(potions[mid] < potionStr){
+
+                l = mid + 1;
+            }else{
+
+                r = mid;
+            }
+        }
+ 
+        return l;
+    }
+
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
 
         vector<int> res;
@@ -11,11 +32,14 @@ public:
             double potionStr = ceil(success/spell); //minimum strength required
 
             //iterator to find the idx of the value equal to or just more than the potionStr using lower_bound on sorted potions array
-            auto itr = lower_bound(potions.begin(), potions.end(), potionStr);
+            //auto itr = lower_bound(potions.begin(), potions.end(), potionStr);
 
-            //alternate -> use user-defined binary search
+            //int countSuccess = potions.end() - itr;
 
-            int countSuccess = potions.end() - itr;
+            //alternate -> use user-defined binary search to find the lower_bound
+            int idx = binarySearch(potions, potionStr);
+
+            int countSuccess =  potions.size() - idx;
 
             res.push_back(countSuccess);
         }
