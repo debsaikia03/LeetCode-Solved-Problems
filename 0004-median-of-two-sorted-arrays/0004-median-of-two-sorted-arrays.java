5c4daa58ae4eba97981b6a7635893ee1f1ff9b8e
Debangshu Saikia <deb.saikia03@gmail.com>
2025-10-27 20:08:27 +0530

    Time: 1 ms (100%), Space: 46.4 MB (12.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,19 +1,28 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        int size = nums1.length + nums2.length;
-        int count = 0;
+        int m = nums1.length;
+        int n = nums2.length;
+        int size = m + n;
+        
         int i = 0, j = 0;
         
-        if (size % 2 == 0) {
-            // Even case
-            int flag[] = new int[2];
-            // Fix 1: Typo (Integer) and initialization logic
-            flag[0] = 0; 
-            flag[1] = 0; 
+        // We use your 'flag' array idea from the 'even' case.
+        // flag[0] will store the element at index (size/2) - 1
+        // flag[1] will store the element at index (size/2)
+        int[] flag = new int[2];
 
-            while (i < nums1.length && j < nums2.length) {
-                flag[0] = flag[1]; // Always store the previous element
+        int count = 0;
+        
+        // We iterate (size / 2) + 1 times to find both elements.
+        while (count <= size / 2) {
+            
+            // 1. Store the *previous* element
+            // This is the same as flag[0] = flag[1]
+            flag[0] = flag[1]; 
+            
+            // 2. Find the *current* smallest element (the new flag[1])
+            if (i < m && j < n) {
                 if (nums1[i] < nums2[j]) {
                     flag[1] = nums1[i];
                     i++;
@@ -21,81 +30,27 @@ class Solution {
                     flag[1] = nums2[j];
                     j++;
                 }
-                count++;
-
-                // Fix 4: Corrected off-by-one check
-                // Fix 3: Corrected integer division
-                if (count == size / 2 + 1) {
-                    return (flag[0] + flag[1]) / 2.0; 
-                }
-            }
-
-            // Handle remaining elements in nums1
-            while (i < nums1.length) {
-                flag[0] = flag[1];
+            } else if (i < m) {
+                // Only nums1 has elements left
                 flag[1] = nums1[i];
                 i++;
-                count++;
-                
-                if (count == size / 2 + 1) {
-                    return (flag[0] + flag[1]) / 2.0;
-                }
-            }
-
-            // Handle remaining elements in nums2
-            while (j < nums2.length) {
-                flag[0] = flag[1];
-                // Fix 2: Typo (j instead of i)
-                flag[1] = nums2[j]; 
+            } else {
+                // Only nums2 has elements left
+                flag[1] = nums2[j];
                 j++;
-                count++;
-
-                if (count == size / 2 + 1) {
-                    return (flag[0] + flag[1]) / 2.0;
-                }
             }
 
-        } else {
-            // Odd case (Your logic here was already correct!)
-            int flag = -1;
-
-            while (i < nums1.length && j < nums2.length) {
-                if (nums1[i] < nums2[j]) {
-                    flag = nums1[i];
-                    i++;
-                } else {
-                    flag = nums2[j];
-                    j++;
-                }
-                count++;
-
-                if (count == size / 2 + 1) {
-                    return (double)flag;
-                }
-            }
-            
-            while (i < nums1.length) {
-                flag = nums1[i];
-                i++;
-                count++;
-
-                if (count == size / 2 + 1) {
-                    return (double)flag;
-                }
-            }
-            
-            while (j < nums2.length) {
-                flag = nums2[j];
-                j++;
-                count++;
-
-                if (count == size / 2 + 1) {
-                    return (double)flag;
-                }
-            }
+            count++;
         }
         
-        // This line should ideally not be reached if inputs are valid
-        return 0.0; 
+        // 3. Now that the loop is done, check if size was even or odd
+        
+        if (size % 2 == 0) {
+            // Even case: return the average of the two elements we tracked
+            return (double)(flag[0] + flag[1]) / 2.0;
+        } else {
+            // Odd case: return the single middle element (the last one found)
+            return (double)flag[1];
+        }
     }
 }
