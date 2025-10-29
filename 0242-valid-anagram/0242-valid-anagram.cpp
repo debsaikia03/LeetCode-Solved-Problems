ddcf4faa21fbaa09821ac7952cd83a62b157046a
Debangshu Saikia <deb.saikia03@gmail.com>
2025-10-29 20:26:53 +0530

    Time: 4 ms (38.33%), Space: 9.8 MB (48.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,7 +3,7 @@ public:
     bool isAnagram(string s, string t) {
 
         if(s.size() != t.size()) return false;
-        
+
         unordered_map<char, int> mpp;
 
         for(int i = 0; i < s.size(); i++){
@@ -13,11 +13,11 @@ public:
 
         for(int i = 0; i < t.size(); i++){
 
-            if(mpp.find(t[i]) == mpp.end()  || mpp[t[i]] == 0) return false;
+            if(mpp.find(t[i]) == mpp.end() || mpp[t[i]] == 0) return false;
 
             mpp[t[i]]--;
         }
-
+        
         return true;
     }
 };
