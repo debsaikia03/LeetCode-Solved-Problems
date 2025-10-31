ffe0c2bb70890873dad6c4787ddceee12d90b227
Debangshu Saikia <deb.saikia03@gmail.com>
2025-10-31 18:53:47 +0530

    Time: 4 ms (26.5%), Space: 26.2 MB (53.66%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        
+        unordered_map<int, int> freq;
+
+        int count = 0;
+
+        vector<int> res;
+
+        for(int num : nums){
+
+            freq[num]++;
+
+            if(freq[num] == 2){
+
+                res.push_back(num);
+            }
+
+            if(res.size() == 2) return res;
+        }
+
+        return {};
+    }
+};
