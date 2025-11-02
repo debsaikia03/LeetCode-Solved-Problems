b05127023248290606d2e495b3220d826ce673cc
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-02 14:48:39 +0530

    Time: 0 ms (100%), Space: 19.6 MB (9.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,62 +1,53 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        //if one of the lists is empty, return the other
+        if (!list1) return list2;
+        if (!list2) return list1;
 
-        if(!list1) return list2;
-        if(!list2) return list1;
+        ListNode* newHead = nullptr; //head of the merged list
+        ListNode* newTail = nullptr; //tail pointer for building merged list
 
-        ListNode* newHead = nullptr;
-        ListNode* newTail = nullptr;
-
-        if(list1->val < list2->val){
-
-            newHead = newTail = list1;
-            list1 = list1->next;
-        }else{
-
+        //initialize the head with the smaller of the two first nodes
+        if (list1->val <= list2->val) {
+            newHead = newTail = list1; //both head and tail point to first node
+            list1 = list1->next;       //move list1 forward
+        } else {
             newHead = newTail = list2;
-            list2 = list2->next;
+            list2 = list2->next;       //move list2 forward
         }
 
-        while(list1 && list2){
-
-            if(list1->val < list2->val){
-
-               newTail->next = list1;
-               newTail = list1;
-               list1 = list1->next;
-            }else{
-
+        //traverse both lists until one becomes empty
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                //attach list1ee's current node to the merged list
+                newTail->next = list1;
+                newTail = list1;       //move the tail forward
+                list1 = list1->next;   //advance list1
+            } else {
+                //attach list2's current node to the merged list
                 newTail->next = list2;
-                newTail = list2;
-                list2 = list2->next;
+                newTail = list2;       //move the tail forward
+                list2 = list2->next;   //advance list2
             }
         }
-        
-        while(list1){
 
-            newTail->next = list1;
-            newTail = list1;
-            list1 = list1->next;
+        //attach any remaining nodes from list1
+        while (list1) {
+            newTail->next = list1;     //link remaining nodes
+            newTail = list1;           //move the tail
+            list1 = list1->next;       //advance list1
         }
 
-        while(list2){
-
-            newTail->next = list2;
-            newTail = list2;
-            list2 = list2->next;
+        //attach any remaining nodes from list2
+        while (list2) {
+            newTail->next = list2;     //link remaining nodes
+            newTail = list2;           //move the tail
+            list2 = list2->next;       //advance list2
         }
 
+        //return the head of the merged list
         return newHead;
     }
 };
