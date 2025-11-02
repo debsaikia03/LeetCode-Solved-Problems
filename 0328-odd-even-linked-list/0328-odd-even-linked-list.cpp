4c2cb883baaf028bb3acbc046d8662617df616c7
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-02 19:17:21 +0530

    Time: 0 ms (100%), Space: 15.6 MB (63.44%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -13,36 +13,36 @@ class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
 
-        // If the list has 0 or 1 node, no rearrangement is needed
+        //if the list has 0 or 1 node, no rearrangement is needed
         if(!head || !head->next) return head;
 
-        // 'odd' will track the current odd node
-        // 'even' will track the current even node
+        //'odd' will track the current odd node
+        //'even' will track the current even node
         ListNode *odd = head, *even = head->next;
 
-        // Save the starting point of even list
-        // This will be attached after odd list ends
+        //save the starting point of even list
+        //this will be attached after odd list ends
         ListNode* evenHead = even;
 
-        // Traverse until even pointer or its next becomes NULL
-        // This ensures we do not go out of bounds
+        //traverse until even pointer or its next becomes NULL
+        //this ensures we do not go out of bounds
         while(even && even->next){
 
-            // Link current odd node to the next odd node
+            //link current odd node to the next odd node
             odd->next = odd->next->next;
 
-            // Link current even node to the next even node
+            //link current even node to the next even node
             even->next = even->next->next;
 
-            // Move odd and even pointers forward
+            //move odd and even pointers forward
             odd = odd->next;
             even = even->next;
         }
 
-        // Attach the even list after the last odd node
+        //attach the even list after the last odd node
         odd->next = evenHead;
         
-        // Head now represents rearranged list
+        //head now represents rearranged list
         return head;
     }
 };
