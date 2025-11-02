97b048d95412e0ae8404f0e018a6241afe93362e
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-02 19:10:42 +0530

    Time: 272 ms (8.51%), Space: 18.6 MB (53.82%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -16,7 +16,7 @@ public:
 
         while(d1 != d2){
 
-            //if one of the pointer reach null, point it to respective head again
+            //if one of the pointer reach null, point it to the respective head again
             d1 = d1 ? d1->next : headA;
             d2 = d2 ? d2->next : headB;
         }
