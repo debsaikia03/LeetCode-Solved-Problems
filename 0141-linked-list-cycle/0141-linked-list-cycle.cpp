4ac0e26eb617a0a776394679c9e847aebb750551
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-02 15:04:32 +0530

    Time: 3 ms (99.07%), Space: 11.7 MB (79.83%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,23 +9,20 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // If the list is empty or has only one node, it can't have a cycle
-        if (!head || !head->next) return false;
 
-        // slow moves 1 step at a time, fast moves 2 steps
-        ListNode* slow = head;
+        if(!head || !head->next) return false;
+        
         ListNode* fast = head;
+        ListNode* slow = head;
 
-        // Continue while fast can take two steps ahead
-        while (fast && fast->next) {
-            slow = slow->next;           // move slow pointer by 1 step
-            fast = fast->next->next;     // move fast pointer by 2 steps
+        while(fast && fast->next){
 
-            // If slow and fast meet, it means there is a cycle
-            if (slow == fast) return true;
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) return true;
         }
 
-        // If fast reaches the end (nullptr), there is no cycle
         return false;
     }
 };
