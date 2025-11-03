32df46dfdfc9e444269f0fb75686819e39392795
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-03 23:23:43 +0530

    Time: 15 ms (12.79%), Space: 28.6 MB (35.37%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,22 +3,26 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> ans(n,INT_MIN);
-        stack<int> stk;
+        vector<int> ans(n,INT_MIN); //initialize answer array
+        stack<int> stk; //stack to store indices
 
+         //loop from 2*n - 1 to 0 to handle circular array
         for(int i = 2 * n - 1; i >= 0; i--){
 
-            int idx = i % n;
+            int idx = i % n; //get circular index
 
+            //while stack not empty and current element >= top stack element
             while(!stk.empty() && nums[stk.top()] <= nums[idx]){
 
-                stk.pop();  
+                stk.pop();  //pop smaller elements
             }
             
+            //if stack not empty then top element is next greater
             if(stk.empty()) ans[idx] = -1;
 
             else ans[idx] = nums[stk.top()];
 
+            //push current index to stack
             stk.push(idx);
         }
 
