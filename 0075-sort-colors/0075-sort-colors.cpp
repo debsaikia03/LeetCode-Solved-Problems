55816e7ea83728b007074a4325bec12f43c9e52a
Debangshu Saikia <deb.saikia03@gmail.com>
2025-11-18 21:44:10 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        quickSortHelper(nums, 0, nums.size() - 1);
-    }
+        int zeros = 0, ones = 0, twos = 0;
 
-private:
-    void quickSortHelper(vector<int>& nums, int left, int right) {
-        if (left >= right) return; // Base case: if the segment is too small, stop
+        for(int num : nums){
 
-        int pivotIndex = partition(nums, left, right); // Partition the array
-        quickSortHelper(nums, left, pivotIndex - 1);   // Sort left part
-        quickSortHelper(nums, pivotIndex + 1, right);  // Sort right part
-    }
-
-    int partition(vector<int>& nums, int left, int right) {
-        int pivot = nums[right]; // Choose the last element as the pivot
-        int i = left - 1;        // Pointer for the smaller element
-
-        for (int j = left; j < right; ++j) {
-            if (nums[j] <= pivot) { // If the current element is smaller than or equal to the pivot
-                ++i;
-                swap(nums[i], nums[j]); // Swap elements
-            }
+            if(num == 0) zeros++;
+            else if(num == 1) ones++;
+            else twos++;
         }
-        swap(nums[i + 1], nums[right]); // Place the pivot in its correct position
-        return i + 1; // Return the index of the pivot
+
+        int i = 0;
+        while(i < nums.size()){
+
+            if(zeros){
+
+               nums[i] = 0;
+               zeros--;
+            }else if(ones){
+
+                nums[i] = 1;
+                ones--;
+            }else{
+
+                nums[i] = 2;
+                twos--;
+            }
+            i++;
+        }
     }
 };
